76465da673aded2f4b652b74ae9d8a7881847f49
José Ricardo <ricardoanimation2@gmail.com>
2023-07-22 16:45:32 -0300

    Add check for null info

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 function adicionarCriatura(footerIndex) {
     let footer = document.getElementById(`footer${footerIndex}`)
+    
     let nomeCriatura = prompt("Insira nome da criatura.")
+    
+    if (nomeCriatura != "" && nomeCriatura != null) {
     let vidaCriatura = prompt("Insira HP da criatura.")
     let armaduraCriatura = prompt("Insira CA da criatura.")
-
-    footer.innerHTML += `<div class="creature-info"><label>${nomeCriatura}</label><input value="${vidaCriatura}" placeholder="HP" type="number" min="0" max="${vidaCriatura}"><label>CA ${armaduraCriatura}</label></div>`
+    
+    if (vidaCriatura != "" && armaduraCriatura != "") {
+        footer.innerHTML += `<div class="creature-info"><label>${nomeCriatura}</label><input value="${vidaCriatura}" placeholder="HP" type="number" min="0" max="${vidaCriatura}"><label>CA ${armaduraCriatura}</label></div>`
+    }
+    }
 }
 
 function removerCriatura(footerIndex) {
