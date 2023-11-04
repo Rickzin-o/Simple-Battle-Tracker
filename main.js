b8eba56bc72bfb31f6a1d24c396ff6d385739b76
José Ricardo <ricardoanimation2@gmail.com>
2023-11-04 17:21:04 -0300

    code for adding multiple creatures

--- main.js
+++ main.js
@@ -8,7 +8,20 @@ function adicionarCriatura(footerIndex) {
     let armaduraCriatura = document.getElementById("input-ca").value
     
         if (vidaCriatura != "" && armaduraCriatura != "") {
-            footer.innerHTML += `<div class="creature-info"><label>${nomeCriatura}</label><input value="${vidaCriatura}" placeholder="HP" type="number" min="0" max="${vidaCriatura}"><label>CA ${armaduraCriatura}</label></div>`
+            let numeroDeCriaturas = Number(document.getElementById("input-num").value)
+            
+            if (numeroDeCriaturas > 1){
+                for (let i = 0; i < numeroDeCriaturas; i++) {
+                    footer.innerHTML += `<div class="creature-info"><label>${nomeCriatura}</label><input value="${vidaCriatura}" placeholder="HP" type="number" min="0" max="${vidaCriatura}"><label>CA ${armaduraCriatura}</label></div>`
+                }
+            } else{
+                footer.innerHTML += `<div class="creature-info"><label>${nomeCriatura}</label><input value="${vidaCriatura}" placeholder="HP" type="number" min="0" max="${vidaCriatura}"><label>CA ${armaduraCriatura}</label></div>`
+            }
+
+            document.getElementById("input-name").value = ""
+            document.getElementById("input-hp").value = ""
+            document.getElementById("input-ca").value = ""
+            document.getElementById("input-num").value = ""
         }
     } else {
         alert("Por favor, insira um nome de criatura válido!")
